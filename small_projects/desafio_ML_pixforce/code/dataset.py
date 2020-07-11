8d88545d105ed9791984a7d18ad1adfdbe706694
Rafael Padilla UFRJ <rafael.padilla@smt.ufrj.br>
2020-07-11 18:28:11 -0300

    Adicionando função para inserir transformações durante run-time.

--- small_projects/desafio_ML_pixforce/code/dataset.py
+++ small_projects/desafio_ML_pixforce/code/dataset.py
@@ -9,7 +9,9 @@ import pickle
 
 import numpy as np
 import torch
+from PIL import Image
 from torch.utils.data import Dataset
+from torchvision import transforms
 from torchvision.transforms import Compose
 
 
@@ -22,7 +24,9 @@ class PixForceDataset(Dataset):
         data = pickle.load(open(ds_file_path, 'rb'))
         self.images = [im.astype(np.uint8) for im in data['samples']]
         self.labels = data['labels']
-        assert len(self.images) == len(self.labels), 'A quantidade de amostras ({len(self.images)}) é diferente da quantidade de rótulos ({len(self.labels)})'
+        assert len(self.images) == len(
+            self.labels
+        ), 'A quantidade de amostras ({len(self.images)}) é diferente da quantidade de rótulos ({len(self.labels)})'
         # Transformações para serem aplicadas na imagem
         self.transformations = transformations
 
@@ -41,3 +45,23 @@ class PixForceDataset(Dataset):
             elif isinstance(self.transformations, Compose):
                 imgs = self.transformations(imgs)
         return {'images': imgs, 'labels': labels}
+
+    def add_transformation(self, new_transformation, begin=True):
+        new_transformations = []
+        # Inclui nova transformacao no começo
+        if begin:
+            new_transformations.append(new_transformation)
+        if isinstance(self.transformations, list):
+            for transformation in self.transformations:
+                new_transformations.append(transformation)
+            # Inclui nova transformacao no final
+            if not begin:
+                new_transformations.append(new_transformation)
+        elif isinstance(self.transformations, Compose):
+            for transformation in self.transformations.transforms:
+                new_transformations.append(transformation)
+            # Inclui nova transformacao no final
+            if not begin:
+                new_transformations.append(new_transformation)
+            new_transformations = transforms.Compose(new_transformations)
+        self.transformations = new_transformations
